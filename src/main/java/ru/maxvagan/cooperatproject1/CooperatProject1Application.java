a43126eb144af435a63daa7e928253fc18c90386
Maxim_Vagan <maxut@gmail.com>
2022-11-04 18:45:19 +0300

    Коммит инициализации проекта

---
+++ src/main/java/ru/maxvagan/cooperatproject1/CooperatProject1Application.java
@@ -0,0 +1,13 @@
+package ru.maxvagan.cooperatproject1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CooperatProject1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CooperatProject1Application.class, args);
+    }
+
+}
---
+++ src/test/java/ru/maxvagan/cooperatproject1/CooperatProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.maxvagan.cooperatproject1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CooperatProject1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
