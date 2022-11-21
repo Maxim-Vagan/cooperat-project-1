2368fc121fea568c3989590cc9dbb538b61ae8f0
Maksik1935 <96553019+Maksik1935@users.noreply.github.com>
2022-11-21 14:27:40 +0300

    Revert "Dev"

--- src/main/java/ru/jd6team7/cooperatproject1/CooperatProject1Application.java
+++ src/main/java/ru/jd6team7/cooperatproject1/CooperatProject1Application.java
@@ -1,20 +1,13 @@
 package ru.jd6team7.cooperatproject1;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@OpenAPIDefinition
 public class CooperatProject1Application {
 
     public static void main(String[] args) {
-        try {
-            SpringApplication.run(CooperatProject1Application.class, args);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
+        SpringApplication.run(CooperatProject1Application.class, args);
     }
 
 }
